949c5f6aa941da7102e3bd47014113d29ae54da4
Chutis <L191200090@tlalpan.tecnm.mx>
2023-03-20 20:44:27 -0600

    add image of my face

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import './Display.css'
 
 const Display = () => {
     return(
         <div className="container xl">
-            <h1 className="text-center my-2 text-white">Hola</h1>
+            <h1 className="text-end my-5 text-white">
+                <h1 className="color-green">Hello My Name is Luis Garcia</h1>
+                <h3 className="mb-4">I make websites</h3>
+                <h6>I'm frot-end developer and I was working in the Institute Technological of Mexico in my social service. 
+                </h6>
+                <h6>I'm a student of the career of Information Technolis</h6>
+            </h1>
         </div>
     )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import './Nav.css'
 import { RxAccessibility} from "react-icons/rx";
 import { BsFlower1 } from "react-icons/bs";
+import { GiAnt } from "react-icons/gi";
+import face from "../../img/portafolio-removebg-preview.png";
 
 
 const Nav = () => {
     return(
-        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark ">
         <div className="container-fluid parrafoNav">
-          <a className="navbar-brand" href="/">L
-          <img src="" alt="" />
+          <a className="navbar-brand" href="/">
+            <img src={face} alt="" width={45} height={55} className="d-inline-block align-text-top circulo"/>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
             <span className="navbar-toggler-icon"></span>
@@ -23,7 +25,7 @@ const Nav = () => {
                 <a className="nav-link" href="/">Link <BsFlower1/></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
+                <a className="nav-link" href="/">Link <GiAnt/></a>
               </li>
             </ul>
           </div>
