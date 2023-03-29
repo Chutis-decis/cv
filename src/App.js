a25f3bfea5c137541ae8553852fcba3ada3df315
Chutis <L191200090@tlalpan.tecnm.mx>
2023-03-29 13:13:45 -0600

    added the footer and three image for the center of the screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Display from './components/display/Display';
 import Nav from './components/nav/Nav'
+import Fotter from './components/footer/Fotter'
 function App() {
   return (
     <div className='App'>
       <Nav/>
       <Display/>
+      <Fotter/>
     </div>
   );
 }
---
+++ src/components/footer/Fotter.jsx
@@ -0,0 +1,24 @@
+import React  from "react";
+import '../nav/Nav.css'
+import '../../App.css'
+//import { FaGithubSquare } from "react-icons/fa";
+
+const Fotter = () => {
+    return(
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    Column
+                </div>
+                <div class="col">
+                    Column
+                </div>
+                <div class="col">
+                    Column
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Fotter;
